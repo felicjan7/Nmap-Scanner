932b1746b7ad8765dc5bad038f5ad01a41435fa2
Krzysztof <wyskokkrzysztof@gmail.com>
2024-12-27 19:24:45 +0100

    Main file with standard/normal for

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] Mainmenu) {
         Scanner menu = new Scanner(System.in);
         System.out.println("Wybierz funkcje");
 
         Object[][] menuTab = new Object[2][2];
         menuTab[0][0] = (byte) 1;
-        menuTab[0][1] = (String) "Instalacja";
+        menuTab[0][1] = "Instalacja";
         menuTab[1][0] = (byte) 2;
-        menuTab[1][1] = (String) "Użycie";
+        menuTab[1][1] = "Użycie";
 
-        for (byte i = 0; i < menuTab.length; i++) {
-            for (byte j = 0; j < menuTab[i].length; j++) {
-                System.out.print(menuTab[i][j] + " ");
-            }
-            System.out.println();
-        }
+//        for (byte i = 0; i < menuTab.length; i++) {
+//            for (byte j = 0; j < menuTab[i].length; j++) {
+//                System.out.print(menuTab[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
         byte menuVar = menu.nextByte();
 
         if (menuVar == (byte) 1) {
             System.out.println("Git");
-            MenuInstall menuInstall = new MenuInstall();
-            menuInstall.menuInstallTest();
+            MenuInstall.InstallationPowerShellScript();
+            System.out.println("Instalacja zakończona, kontynuujemy w Main");
+
         } else if ( menuVar == (byte) 2) {
             System.out.println("NieGit");
         } else {
             System.out.println("Dupa");
         }
-
+         //Main.main(Mainmenu);
 
     } // method
 }// class
